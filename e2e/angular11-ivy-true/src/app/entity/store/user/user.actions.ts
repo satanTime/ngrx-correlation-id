7549fb94e2c595d8f3ae6901243c98724214c7d0
MG <m@sudo.eu>
2020-12-26 23:16:37 +0100

    build(a11): movement

--- e2e/angular11-ivy-true/src/app/entity/store/user/user.actions.ts
+++
@@ -1,6 +0,0 @@
-import {createAction, props} from '@ngrx/store';
-
-import {User} from './user.model';
-
-export const loadUser = createAction('[User] Load User', props<{cid: string; fail: boolean}>());
-export const upsertUser = createAction('[User] Upsert User', props<{user: User}>());
