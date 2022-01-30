ac7a45d2c3b07af2dd2c2f1db1c78874cf940c7e
satanTime <m@sudo.eu>
2022-01-30 14:42:23 +0100

    build(a13): checking on a13

---
+++ e2e/a13/src/app/entity/store/reducers.ts
@@ -0,0 +1,16 @@
+import {ActionReducerMap, createFeatureSelector, createSelector} from '@ngrx/store';
+
+import * as fromUser from './user/user.reducer';
+
+export interface State {
+  users: fromUser.State;
+}
+
+export const reducers: ActionReducerMap<State> = {
+  users: fromUser.reducer,
+};
+
+// feature selectors
+export const selectUserState = createFeatureSelector<fromUser.State>('users');
+
+export const selectUsers = createSelector(selectUserState, fromUser.adapter.getSelectors().selectAll);
---
+++ e2e/a13/src/app/entity/store/user/user.reducer.ts
@@ -0,0 +1,19 @@
+import {createEntityAdapter, EntityAdapter, EntityState} from '@ngrx/entity';
+import {Action, createReducer, on} from '@ngrx/store';
+import {upsertUser} from './user.actions';
+import {User} from './user.model';
+
+export interface State extends EntityState<User> {}
+
+export const adapter: EntityAdapter<User> = createEntityAdapter<User>();
+
+export const initialState: State = adapter.getInitialState();
+
+const internalReducer = createReducer(
+  initialState,
+  on(upsertUser, (state, {user}) => adapter.upsertOne(user, state)),
+);
+
+export function reducer(state = initialState, action: Action) {
+  return internalReducer(state, action);
+}
